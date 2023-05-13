2d8f46a0106d82c6b6b824f7741ff397dc2c0096
clever-girl31 <johnmseybold@gmail.com>
2023-05-13 01:14:37 -0500

    set up first get route

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+)
+
+app.listen(PORT, () =>
+  console.log(`App listening at localhost:${PORT}`)
+);
